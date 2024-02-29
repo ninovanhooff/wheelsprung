52371f59b091bf786f41d5a4bce3bd93fe5dfc78
Nino van Hooff <nino@q42.nl>
2024-02-29 21:09:00 +0100

    FIX glitchy debug camera

--- src/screens/game/game_screen.nim
+++ src/screens/game/game_screen.nim
@@ -276,7 +276,7 @@ method update*(gameScreen: GameScreen): int {.locks:0.} =
 
   state.camera = state.level.cameraBounds.clampVect(
     state.chassis.position - halfDisplaySize
-  )
+  ).round()
   drawGame(addr state) # todo pass as object?
   return 1
 
