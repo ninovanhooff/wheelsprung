52510c0ca62af78db5a516646cc34824618a8594
Nino van Hooff <nino@q42.nl>
2024-03-02 23:02:46 +0100

    ADD forceIntegerCoordinates.js

---
+++ support/editor/extensions/forceIntegerCoordinates.js
@@ -0,0 +1,42 @@
+function forceIntegerCoordinates(mapOrLayer) {
+	tiled.log("Running forceIntegerCoordinates:" + mapOrLayer);
+	if(!mapOrLayer){
+		return;
+	};
+	if(mapOrLayer.isObjectLayer) {
+		let objects = mapOrLayer.objects;
+		for(object of objects) {
+			object.x = Math.round(object.x);
+			object.y = Math.round(object.y);
+			object.width = Math.round(object.width);
+			object.height = Math.round(object.height);
+			if(object.shape == MapObject.Polygon || object.shape == MapObject.Polyline) {
+				object.polygon = object.polygon.map((p) => 
+					Qt.point(
+						Math.round(p.x), 
+						Math.round(p.y)
+					)
+				); 
+			}
+		}
+	} else if(mapOrLayer.isTileMap || mapOrLayer.isGroupLayer) {
+		let numLayers = mapOrLayer.layerCount;
+		for(var i = 0; i < numLayers; i++) {
+			forceIntegerCoordinates(mapOrLayer.layerAt(i));
+		}
+	} else {
+		//else, do nothing
+	}
+		
+}
+
+//Auto-apply on save:
+tiled.assetAboutToBeSaved.connect(function(asset) {if(asset.isTileMap) forceIntegerCoordinates(asset); } );
+
+//Allow manually applying via an Action:
+let forceIntegerCoordinatesAction = tiled.registerAction("ForceIntegerCoordinates", function() { forceIntegerCoordinates(tiled.activeAsset); } );
+forceIntegerCoordinatesAction.text = "Force Integer Coordinates";
+//add this action to the Edit menu:
+tiled.extendMenu("Edit", [
+	{ action: "ForceIntegerCoordinates", before: "Preferences" }
+]);
