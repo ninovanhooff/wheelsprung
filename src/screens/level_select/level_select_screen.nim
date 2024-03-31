0482a02f45b5ebd08daeec4421c1167200448357
Nino van Hooff <nino@q42.nl>
2024-03-31 10:32:31 +0200

    ADD scrolling to Level Select screen

--- src/screens/level_select/level_select_screen.nim
+++ src/screens/level_select/level_select_screen.nim
@@ -1,4 +1,5 @@
 import playdate/api
+import sequtils
 import navigation/[screen, navigator]
 import graphics_types
 import utils
@@ -7,18 +8,27 @@ import configuration
 import screens/game/game_screen
 import screens/settings/settings_screen
 
-const borderInset = 24
-const levelsBasePath = "levels/"
+const 
+  borderInset = 24
+  levelsBasePath = "levels/"
+  maxLines = 8 
 
 type LevelSelectScreen = ref object of Screen
   levelPaths: seq[string]
   selectedIndex: int
+  scrollPosition: int
 
 proc getLevelPaths(): seq[string] =
   playdate.file.listFiles(levelsBasePath)
 
 proc newLevelSelectScreen*(): LevelSelectScreen =
   return LevelSelectScreen(screenType: ScreenType.LevelSelect)
+
+proc updateScrollPosition(screen: LevelSelectScreen) =
+  if screen.selectedIndex < screen.scrollPosition:
+    screen.scrollPosition = screen.selectedIndex
+  elif screen.selectedIndex > screen.scrollPosition + maxLines - 1:
+    screen.scrollPosition = screen.selectedIndex - maxLines + 1
 
 proc updateInput(screen: LevelSelectScreen) =
   let buttonState = playdate.system.getButtonState()
@@ -42,6 +52,8 @@ proc updateInput(screen: LevelSelectScreen) =
     if screen.selectedIndex >= screen.levelPaths.len:
       screen.selectedIndex = 0
 
+  updateScrollPosition(screen)
+
 proc drawBackground() =
   gfx.drawRect(borderInset, borderInset, 400 - 2 * borderInset, 240 - 2 *
       borderInset, kColorBlack)
@@ -51,11 +63,15 @@ proc drawTitle(title: string) =
 
 proc drawLevelPaths(screen: LevelSelectScreen) =
   var y = 40
-  for level in screen.levelPaths:
+  let maxIdx = clamp(
+    screen.scrollPosition + maxLines - 1, 
+    0, screen.levelPaths.high
+  )
+  for level in screen.levelPaths[screen.scrollPosition .. maxIdx]:
     gfx.drawText(level, borderInset * 2, y)
     y += 20
-
-  gfx.drawText(">", borderInset + 8, 40 + screen.selectedIndex * 20)
+  let cursorY = 40 + 20 * (screen.selectedIndex - screen.scrollPosition)
+  gfx.drawText(">", borderInset + 8, cursorY)
 
 proc drawButtons(screen: LevelSelectScreen) =
   let selectedFileName = screen.levelPaths[screen.selectedIndex]
