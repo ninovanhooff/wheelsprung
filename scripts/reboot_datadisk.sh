1c6f11186d415238844ee07269a600221d948b9f
Nino van Hooff <ninovanhooff@gmail.com>
2024-04-06 22:38:12 +0200

    ADD pre-check for datadisk to reboot_datadisk.sh

--- scripts/reboot_datadisk.sh
+++ scripts/reboot_datadisk.sh
@@ -3,6 +3,13 @@
 # Put device in data disk mode. 
 # Does not wait for mounting, but waits for the device to be connected
 # Do this first because it takes many seconds. We can compile while mounting
+
+# first, check if the device is already in data disk mode
+if [ -e /Volumes/PLAYDATE ]; then
+  echo "Playdate is already in data disk mode"
+  exit 0
+fi
+
 until ls /dev/cu.usbmodemPD*
 do
   echo "Playdate not found. Is it connected to USB and unlocked?"
