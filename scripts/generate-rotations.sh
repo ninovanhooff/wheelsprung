4ba8c2ae597abbaf7e5f3a35e06e5fc57c9f83fb
Nino van Hooff <ninovanhooff@gmail.com>
2024-04-18 21:35:53 +0200

    CHANGE bike chassis to 48x48 design by Bobby (auto rotated)

--- scripts/generate-rotations.sh
+++ scripts/generate-rotations.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-spriterot -v -r 64 -o ./wheel-table-22-22.png ./wheel.png
+spriterot -v -r 64 -o ../../source/images/bike-wheel-table-22-22.png ./wheel.png
+spriterot -v -w 48 -h 48 -k -r 64 -o ../../source/images/bike-chassis-table-48-48.png ./bike-chassis.png
 
 spriterot -v -r 64 -o ../../source/images/rider/upper-arm-table-14-14.png ./rider/upper-arm.png
 spriterot -v -r 64 -o ../../source/images/rider/lower-arm-table-12-12.png ./rider/lower-arm.png
 spriterot -v -r 64 -o ../../source/images/rider/upper-leg-table-18-18.png ./rider/upper-leg.png
 spriterot -v -r 64 -o ../../source/images/rider/lower-leg-table-14-14.png ./rider/lower-leg.png
+spriterot -v -r 64 -o ../../source/images/rider/torso-table-18-18.png ./rider/torso.png
 spriterot -v -r 64 -o ../../source/images/rider/head-table-14-14.png ./rider/head.png
