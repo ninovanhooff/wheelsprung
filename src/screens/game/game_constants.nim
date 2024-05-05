0d1d5d394f50e79cc44f4199787d0e45978558e6
Nino van Hooff <ninovanhooff@gmail.com>
2024-05-05 21:39:32 +0200

    FIX timeStepSeconds64 based on timeStep

--- src/screens/game/game_constants.nim
+++ src/screens/game/game_constants.nim
@@ -7,9 +7,9 @@ const
   # this is better than letting the physics engine run at a variable rate
   # which can cause instability.
   # Also, this might be desirable for slow motion effects.
-  physicsTickRate* = 50
+  physicsTickRate* = 45
   timeStep*: int32 = 1000.Milliseconds div physicsTickRate
-  timeStepSeconds64*: float64 = 1.0 / physicsTickRate.float64
+  timeStepSeconds64*: float64 = timeStep.float64 / 1_000.0'f64
     ## timeStepSeconds64 is used to advance the physics simulation, let's go for maximum precision to minimise drift
   riderOffset* = v(-4.0, -18.0) # offset from chassis center
   HALF_PI*: float = PI * 0.5
