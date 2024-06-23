8b1434c8a3edf3a4dd025f8109fdcb5415e040d4
Nino van Hooff <ninovanhooff@gmail.com>
2024-06-23 22:09:36 +0200

    CHANGE tiled extension to ts and register wmj format

---
+++ support/editor/extensions/src/wheelsprung-map.ts
@@ -0,0 +1,32 @@
+
+const mapFormat: ScriptedMapFormat = {
+  name: "wheelsprung",
+  extension: "wmj",
+  write: function(map: TileMap, fileName: string) {
+    // const defaults = getProjectPropertiesDefaults();
+    // const actions = JSON.stringify(tiled.actions, getCircularReplacer(), 2);
+    // tiled.log(`actions: ${actions}`)
+    // tiled.log(`Defaults: ${JSON.stringify(defaults, getCircularReplacer(), 2)}`);
+    // printObjectGroups(map, defaults);
+    
+    // tiled.log(JSON.stringify(map.layers[0].className, getCircularReplacer(), 2));
+    // tiled.trigger("Export");
+    // const mapJson = JSON.stringify(map, null, 2);
+    // const file = new TextFile(fileName, TextFile.WriteOnly);
+    // file.write("hoi");
+    // file.commit();
+
+    tiled.mapFormatForFile("test.tmj").write(map, fileName);
+
+    return undefined; // success
+  },
+  read: function(fileName: string): TileMap {
+    const file = new TextFile(fileName, TextFile.ReadOnly);
+    const mapJson = file.readAll();
+    file.close();
+
+    return JSON.parse(mapJson);
+  },
+};
+
+export default mapFormat;
