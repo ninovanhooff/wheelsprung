beeb7772f253257e2a04ee656333eac2871aa156
Nino van Hooff <ninovanhooff@gmail.com>
2024-09-04 22:53:01 +0200

    ADD convert_assets.bat

---
+++ scripts/convert_assets.bat
@@ -0,0 +1,24 @@
+@echo off
+
+REM Store the script's directory to a variable
+set "SCRIPT_DIR=%~dp0"
+
+REM Remove the directory temp.pdx inside SCRIPT_DIR
+rmdir /s /q "%SCRIPT_DIR%temp.pdx"
+
+echo Script directory: %SCRIPT_DIR%
+
+REM Create an empty file called temp.lua in the script directory
+copy nul "%SCRIPT_DIR%temp.lua" >nul
+
+echo Converting assets...
+
+pdc -k -m temp.lua "%SCRIPT_DIR%\"
+
+REM Remove the temp.lua file
+del "%SCRIPT_DIR%temp.lua"
+
+echo DONE
+echo Assets can be found in the temp.pdx directory
+
+pause
