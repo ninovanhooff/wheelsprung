c6429f6de7d4f5372ba34d95adb119511a71dc0a
Nino van Hooff <ninovanhooff@gmail.com>
2024-09-08 22:08:12 +0200

    FIX: Crash on Gravity Zone collision

--- src/screens/game/game_gravity_zone.nim
+++ src/screens/game/game_gravity_zone.nim
@@ -1,14 +1,13 @@
 import chipmunk7
 import common/graphics_utils
 import game_types
-import common/utils
 
 const 
   gravityZoneRadius = 20.0.Float
   vGravityZoneCenterOffset = v(gravityZoneRadius, gravityZoneRadius)
 
 proc addGravityZones*(space: Space, gravityZones: seq[GravityZone]) =
-  for index, gravityZone in gravityZones:
+  for gravityZone in gravityZones:
     let vCenter = gravityZone.position.toVect + vGravityZoneCenterOffset
     let shape = space.addShape(space.staticBody.newCircleShape(gravityZoneRadius, vCenter))
     shape.sensor = true # only detect collisions, don't apply forces to colliders
@@ -16,12 +15,15 @@ proc addGravityZones*(space: Space, gravityZones: seq[GravityZone]) =
     shape.filter = GameShapeFilters.GravityZone
     shape.userData = cast[DataPointer](gravityZone)
 
+let gravityZonePostStepCallback: PostStepFunc = proc(space: Space, gravityShape: pointer, unused: pointer) {.cdecl raises: [].} =
+  let gravityShape = cast[Shape](gravityShape)
+  let gravityZone = cast[GravityZone](gravityShape.userData)
+  echo "hit gravity zone:" & repr(gravityZone)
+  let newGravity = gravityZone.gravity
+  space.gravity = newGravity
 
 let gravityZoneBeginFunc*: CollisionBeginFunc = proc(arb: Arbiter; space: Space; unused: pointer): bool {.cdecl.} =
-  var 
-    shapeA: Shape
-    shapeB: Shape
+  var shapeA, shapeB: Shape
   arb.shapes(addr(shapeA), addr(shapeB))
-  space.gravity=cast[GravityZone](shapeA.userData).gravity
-  print "new gravity", space.gravity
-  return false # don't process the collision further
+  discard space.addPostStepCallback(gravityZonePostStepCallback, shapeA, nil)
+  return false
