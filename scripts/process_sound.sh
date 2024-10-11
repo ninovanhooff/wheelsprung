8afc3473915d2ea282037fe406e0daf7156788a4
Nino van Hooff <ninovanhooff@gmail.com>
2024-10-11 14:47:19 +0200

    CHANGE process_sound.sh to be able to process multiple directories

--- scripts/process_sound.sh
+++ scripts/process_sound.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-# Set the input and output directories
-input_dir="support/sounds/thud"
-output_dir="source/audio/thud"
-
 echo "Additional arg passed to adpcm-xq: $1"
 
-# Loop through each wav file in the input directory
-for file in "$input_dir"/*.wav; do
-  # Get the filename
-  filename=$(basename "$file")
-  
-  # Call the adpcm-xq program with the input and output filenames
-  adpcm-xq -e "$1" "$file" "$output_dir/$filename"
-done
+process_sounds() {
+  local input_dir="$1"
+  local output_dir="$2"
+  local additional_arg="$3"
+
+  # Loop through each wav file in the input directory
+  for file in "$input_dir"/*.wav; do
+    # Get the filename
+    filename=$(basename "$file")
+    
+    # Call the adpcm-xq program with the input and output filenames
+    adpcm-xq -e "$additional_arg" "$file" "$output_dir/$filename"
+  done
+}
+
+# Call the function with the specified input and output directories
+process_sounds "support/sounds/thud" "source/audio/thud" "$1"
+#process_sounds "support/music" "source/audio/music" "$1"
