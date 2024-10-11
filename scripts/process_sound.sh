3ed1bbfbad4f4d4087263e6937ed75d816d9d107
Nino van Hooff <ninovanhooff@gmail.com>
2024-10-11 14:53:00 +0200

    ADD soundtrack support file

--- scripts/process_sound.sh
+++ scripts/process_sound.sh
@@ -19,4 +19,6 @@ process_sounds() {
 
 # Call the function with the specified input and output directories
 process_sounds "support/sounds/thud" "source/audio/thud" "$1"
+
+# when uncommenting make sure no doubles wav files for music that is used as mpe)
 #process_sounds "support/music" "source/audio/music" "$1"
