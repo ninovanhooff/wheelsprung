76ea08ec620ea02051cca0e50f3b81d3649d3692
Nino van Hooff <ninovanhooff@gmail.com>
2024-11-17 14:12:35 +0100

    ADD some parameters to polyline generator

--- editor/extensions/src/expression-to-polygon.ts
+++ editor/extensions/src/expression-to-polygon.ts
@@ -11,30 +11,67 @@ function newPolyline(posX: number, posY: number, polygon: point[]) {
   return object;
 }
 
-export function evalDemo(posX: number, posY: number, resolution: number = 10, expressionX: string = "t", expressionY: string) {
+export function evalDemo(posX: number, posY: number, numPoints: number = 10, expressionX: string = "t", expressionY: string, epsilon: number = 0.5) {
   
   // let runnable :any = eval(result);
   // runnable.Run("RUN!").then((result:string)=>{tiled.log(result);});
   
 
-  let resultsX = evaluateExpression(expressionX, resolution);
-  let resultsY = evaluateExpression(expressionY, resolution);
+  let resultsX = evaluateExpression(expressionX, numPoints);
+  let resultsY = evaluateExpression(expressionY, numPoints);
   let points: point[] = resultsX.map((x, i) => ({ x, y: resultsY[i] }));
-  let polygon = newPolyline(posX, posY, points);
+  let polygon = newPolyline(posX, posY, optimizePoints(points, epsilon));
+  tiled.log("Optimized polygon: " + polygon.polygon.length);
   this.activeAsset.currentLayer.addObject(polygon);
 }
 
-function evaluateExpression(expression: string, resolution: number): number[] {
+function evaluateExpression(expression: string, numPoints: number): number[] {
   tiled.log("Evaluating expression: " + expression);
   const sanitizedExpression = expression.replace(/[^-()\d/*+.\w\^]/g, '');
-  // if (sanitizedExpression !== expression) {
-    tiled.log("Sanitized expression: " + sanitizedExpression);
-  // }
-  return Array.from({ length: resolution }, (_, i) => {
+  tiled.log("Sanitized expression: " + sanitizedExpression);
+  return Array.from({ length: numPoints }, (_, i) => {
     let t = i;
     let replacedExpression = eval(sanitizedExpression.replace("t", t.toString()));
     let result = eval(replacedExpression);
     tiled.log(result);
     return result;
   });
+}
+
+/**
+ * Optimizes the points by removing straight lines using the Ramer-Douglas-Peucker algorithm
+ * @param points 
+ * @returns 
+ */
+function optimizePoints(points: point[], epsilon: number = 0.5): point[] {
+  if (points.length < 3) {
+    return points;
+  }
+  var dMax = 0.0;
+  let dMaxIndex = 0;
+  for (let i = 1; i < points.length - 1; i++) {
+    let d = perpendicularDistance(points[i], points[0], points[points.length - 1]);
+    if (d > dMax) {
+      dMax = d;
+      dMaxIndex = i;
+    }
+  }
+  if (dMax > epsilon) {
+    const left = optimizePoints(points.slice(0, dMaxIndex + 1), epsilon);
+    const right = optimizePoints(points.slice(dMaxIndex), epsilon);
+    return left.slice(0, -1).concat(right);
+  } else {
+    return [points[0], points[points.length - 1]];
+  }
+}
+
+function perpendicularDistance(arg0: point, arg1: point, arg2: point) {
+  const { x: x1, y: y1 } = arg1;
+  const { x: x2, y: y2 } = arg2;
+  const { x: x0, y: y0 } = arg0;
+
+  const numerator = Math.abs((y2 - y1) * x0 - (x2 - x1) * y0 + x2 * y1 - y2 * x1);
+  const denominator = Math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2);
+
+  return numerator / denominator;
 }
--- editor/extensions/wheelsprung/index.js
+++ editor/extensions/wheelsprung/index.js
@@ -73,24 +73,54 @@
     object.polygon = polygon;
     return object;
   }
-  function evalDemo(posX, posY, resolution2 = 10, expressionX = "t", expressionY) {
-    let resultsX = evaluateExpression(expressionX, resolution2);
-    let resultsY = evaluateExpression(expressionY, resolution2);
+  function evalDemo(posX, posY, numPoints2 = 10, expressionX = "t", expressionY, epsilon = 0.5) {
+    let resultsX = evaluateExpression(expressionX, numPoints2);
+    let resultsY = evaluateExpression(expressionY, numPoints2);
     let points = resultsX.map((x, i2) => ({ x, y: resultsY[i2] }));
-    let polygon = newPolyline(posX, posY, points);
+    let polygon = newPolyline(posX, posY, optimizePoints(points, epsilon));
+    tiled.log("Optimized polygon: " + polygon.polygon.length);
     this.activeAsset.currentLayer.addObject(polygon);
   }
-  function evaluateExpression(expression, resolution) {
+  function evaluateExpression(expression, numPoints) {
     tiled.log("Evaluating expression: " + expression);
     const sanitizedExpression = expression.replace(/[^-()\d/*+.\w\^]/g, "");
     tiled.log("Sanitized expression: " + sanitizedExpression);
-    return Array.from({ length: resolution }, (_, i) => {
+    return Array.from({ length: numPoints }, (_, i) => {
       let t = i;
       let replacedExpression = eval(sanitizedExpression.replace("t", t.toString()));
       let result = eval(replacedExpression);
       tiled.log(result);
       return result;
     });
+  }
+  function optimizePoints(points, epsilon = 0.5) {
+    if (points.length < 3) {
+      return points;
+    }
+    var dMax = 0;
+    let dMaxIndex = 0;
+    for (let i2 = 1; i2 < points.length - 1; i2++) {
+      let d = perpendicularDistance(points[i2], points[0], points[points.length - 1]);
+      if (d > dMax) {
+        dMax = d;
+        dMaxIndex = i2;
+      }
+    }
+    if (dMax > epsilon) {
+      const left = optimizePoints(points.slice(0, dMaxIndex + 1), epsilon);
+      const right = optimizePoints(points.slice(dMaxIndex), epsilon);
+      return left.slice(0, -1).concat(right);
+    } else {
+      return [points[0], points[points.length - 1]];
+    }
+  }
+  function perpendicularDistance(arg0, arg1, arg2) {
+    const { x: x1, y: y1 } = arg1;
+    const { x: x2, y: y2 } = arg2;
+    const { x: x0, y: y0 } = arg0;
+    const numerator = Math.abs((y2 - y1) * x0 - (x2 - x1) * y0 + x2 * y1 - y2 * x1);
+    const denominator = Math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2);
+    return numerator / denominator;
   }
 
   // src/wheelsprung-map.ts
