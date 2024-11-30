7c214d06d052573809dc9dad7fb035627606454b
Nino van Hooff <ninovanhooff@gmail.com>
2024-11-30 16:02:25 +0100

    ADD also sync images in sync_levels.sh

--- scripts/sync_levels.sh
+++ scripts/sync_levels.sh
@@ -8,11 +8,14 @@ SOURCE_DIR="$SCRIPT_DIR/../source/levels"
 DEST_DIR="$HOME/Developer/PlaydateSDK/Disk/Data/com.ninovanhooff.wheelsprung"
 alias run_rsync='rsync -azP --exclude ".*/" --exclude ".*" --exclude "tmp/" $SOURCE_DIR $DEST_DIR'
 alias run_level_updater="$SCRIPT_DIR/run_level_updater.sh"
+alias compile_images="$SCRIPT_DIR/compile_images.sh"
 
 run_rsync
 run_level_updater
+compile_images
 
 fswatch -o "$SOURCE_DIR" | while read -r f; do
   run_rsync
   run_level_updater
+  compile_images
 done
