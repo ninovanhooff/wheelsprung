12a6b29b197f0da56ee98a58c9293e68d013d591
Nino van Hooff <ninovanhooff@gmail.com>
2024-12-14 12:52:12 +0100

    ADD zip step to bundle_nolevels.sh

--- scripts/bundle_nolevels.sh
+++ scripts/bundle_nolevels.sh
@@ -13,3 +13,10 @@ destination_dir="${PRODUCT}_nolevels.pdx"
 echo "Bundle simulator PDX without levels from source_dir ${source_dir} to destination_dir ${destination_dir}"
 
 rsync -avrti --delete --prune-empty-dirs --exclude="levels" "$source_dir/" "$destination_dir"
+
+# Remove the existing zip file if it exists
+# this is done because the zip command will append to the existing zip file
+# and we want to create a new zip file
+rm -f "${destination_dir}.zip"
+# zip the destination directory
+zip -r "${destination_dir}.zip" "$destination_dir"
