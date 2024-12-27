359bd892ff2517c7a24ae9f1061525835076e91d
Anders Bjørnskov <andersnormannmadsen@gmail.com>
2024-12-27 19:23:21 +0100

    Initial commit with template

---
+++ sequence1.lua
@@ -0,0 +1,16 @@
+Sequence1 = {
+    -- Sequence 1
+    title = "Chapter 1",
+    panels = { -- a list of PANELS for Sequence 1
+        {    -- Panel 1
+            layers = {
+                -- list of layers for panel 1
+            }
+        },
+        { -- Panel 2
+            layers = {
+                -- list of layers for panel 2
+            }
+        }
+    }
+}
---
+++ sequence2.lua
@@ -0,0 +1,15 @@
+Sequence2 = { -- Sequence 2
+    title = "Chapter 2",
+    panels = { -- a list of PANELS for Sequence 2
+        {     -- Panel 1
+            layers = {
+                -- list of layers for panel 1
+            }
+        },
+        { -- Panel 2
+            layers = {
+                -- list of layers for panel 2
+            }
+        }
+    }
+}
