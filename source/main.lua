e3160ff044e4525b79ac86992f5a9cdff6fefb68
Nino van Hooff <ninovanhooff@gmail.com>
2024-12-28 16:16:44 +0100

    ADD documentation for workaround

--- source/main.lua
+++ source/main.lua
@@ -32,6 +32,8 @@ function StartPanelsExample(finishCallback)
 end
 
 function UpdatePanels()
+    -- workaround for inputHandlers not called when C code is running
+    -- https://devforum.play.date/t/lua-inputhandlers-not-called-when-both-c-updatecallback-is-defined/20745
     local crankChange, acceleratedChange = playdate.getCrankChange()
     if crankChange ~= 0 then
         Panels.cranked(crankChange, acceleratedChange)
