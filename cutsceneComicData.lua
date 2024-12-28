625c053b4fe72549e994fb04afcd1d7ed5cd6966
Nino van Hooff <ninovanhooff@gmail.com>
2024-12-28 12:30:57 +0100

    REFACTOR assets to cutscene subfolders

---
+++ cutsceneComicData.lua
@@ -0,0 +1,10 @@
+-- all the data for your comic goes into this table
+-- you can split it up into multiple files to make it easier to manage 
+
+import "sequence1.lua"
+import "sequence2.lua"
+
+cutsceneComicData = { -- a list of SEQUENCES for this comic
+	Sequence1,
+	Sequence2
+}
