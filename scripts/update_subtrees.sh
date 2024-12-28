09dcb848a304329c4dae12aeac27ad6e86b08682
Nino van Hooff <ninovanhooff@gmail.com>
2024-12-28 14:58:53 +0100

    some update_subtrees changes
    UPDATE update_subtrees

--- scripts/update_subtrees.sh
+++ scripts/update_subtrees.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+set -e
 
 # WIP
-# Perhaps pull the wheelsprung-panels repo first?
+
+# cd to the panels repo
+cd "${SCRIPT_DIR}/../../wheelsprung-panels"
+
+# Perhaps delete the subtrees
+
+
+# recreate the subtrees
+# echo "Splitting subtree for comicData..."
+git subtree split --prefix=source/comicData -b subtree-comicdata 
+echo "Subtree for comicData split into branch 'subtree-comicdata'"
+# git subtree split --prefix=source/images/cutscenes -b subtree-images
+
+# cd back to the nim repo
+echo "Pulling subtrees..."
+cd "${SCRIPT_DIR}/.."
+
+# pull the subtrees
 git subtree pull --prefix=source/comicData wheelsprung-panels subtree-comicdata
+git subtree pull --prefix=source/images/cutscenes wheelsprung-panels subtree-images
