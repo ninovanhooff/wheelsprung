c5008dcf9a1323b7813099b01ae347594f41be32
Anders Bjørnskov <andersnormannmadsen@gmail.com>
2025-01-09 17:11:12 +0100

    Added images to the intro - it just need a bit of refinements

--- intro.lua
+++ intro.lua
@@ -29,6 +29,155 @@ Intro = {
 
             }
         },
+
+        { -- Panel 4
+            layers = {
+                { image = "Intro/E/road.png",     parallax = 0.7 },
+                { image = "Intro/E/Emily.png",  parallax = 0.6 },
+                { image = "Intro/E/Dad.png", parallax = 0.6 },
+                { image = "Intro/E/plants.png",    parallax = 0.1 },
+                {
+                    text = "*This is the best day of   my life!*",
+                    x = 285, y = 15,
+                    rect =  {width = 90, height = 123},
+                    parallax = 0.4
+                },
+
+            }
+        },
+
+        { -- Panel 5
+        layers = {
+            { image = "Intro/F/House.png",  parallax = 0.6 },
+            { image = "Intro/F/Flip.png", parallax = 0.3 },
+            {
+                text = "*It will be the best day of my life.*",
+                x = 290, y = 15,
+                rect =  {width = 90, height = 123},
+                parallax = 0.3
+            },
+
+        }
+    },
+
+            { -- Panel 6
+            layers = {
+                { image = "Intro/G/Background.png",  parallax = 0.6 },
+                { image = "Intro/G/Emily.png", parallax = 0.3 },
+                {
+                    text = "*I love NUTS*",
+                    x = 300, y = 35,
+                    rect =  {width = 50, height = 123},
+                    parallax = 0.3
+                },
+
+            }
+            },
+
+
+            { -- Panel 7
+            layers = {
+                { image = "Intro/H/Kitchen.png",  parallax = 0.6 },
+                { image = "Intro/H/Emily.png", parallax = 0.3 },
+
+            }
+            },
+            {      -- Panel 8
+            layers = {
+                { image = "Intro/I/Bathroom.png",     parallax = 1 },
+                { image = "Intro/I/Emily.png",  parallax = 0.8 },
+                { image = "Intro/I/Nuts.png", parallax = 0.6 },
+                { image = "Intro/I/Towel.png",    parallax = 0.1 },
+            }
+        },
+        {      -- Panel 9
+            layers = {
+                { image = "Intro/J/Emily.png",     parallax = 0.8 },
+                { image = "Intro/J/Window.png",  parallax = 0.7 },
+                { image = "Intro/J/Tree.png",    parallax = 0.1 },
+            }
+        },
+        {      -- Panel 10
+        layers = {
+            { image = "Intro/K/House.png",     parallax = 0.8 },
+            { image = "Intro/K/FLip.png",  parallax = 0.75 },
+            { image = "Intro/K/Plants.png",    parallax = 0.1 },
+        }
+    },
+    {      -- Panel 11
+    layers = {
+        { image = "Intro/L/Room.png",     parallax = 1 },
+        { image = "Intro/L/Nuts.png",     parallax = 1 },
+        { image = "Intro/L/FLip.png",  parallax = 0.8 },
+        { image = "Intro/L/Planet.png",    parallax = 0.1 },
+        {
+            text = "*How can I get them fast before she’s back?*",
+            x = 290, y = 130,
+            rect =  {width = 90, height = 123},
+            parallax = 0.3
+        },
+    }
+},
+    {      -- Panel 12
+    layers = {
+        { image = "Intro/M/Bike.png",     parallax = 1 },
+        { image = "Intro/M/Ahh.png",  parallax = 0.6 },
+        { image = "Intro/M/FLip.png",  parallax = 0.5 },
+        { image = "Intro/M/Rocket.png",    parallax = 0.1 },
+    }
+    },
+
+    {      -- Panel 13
+    layers = {
+        { image = "Intro/N/Bike.png",     parallax = 0.7 },
+        { image = "Intro/N/Flip.png",  parallax = 0.6 },
+        { image = "Intro/N/Stars.png",  parallax = 0.5 },
+        { image = "Intro/N/Front.png",    parallax = 0.1 },
+    }
+    },
+    {      -- Panel 14
+    layers = {
+        { image = "Intro/D/Flip1.png",     parallax = 0.5 },
+        {
+            text = "*That could work!*",
+            x = 290, y = 45,
+            rect =  {width = 90, height = 123},
+            parallax = 0.5
+        },
+
+      
+    },
+},
+    {      -- Panel 15
+    layers = {
+        { image = "Intro/O/Background.png",     parallax = 1 },
+        { image = "Intro/O/Flip.png",  parallax = 0.6 },
+        { image = "Intro/O/Stars.png",  parallax = 0.8 },
+        { image = "Intro/O/Speech.png",    parallax = 0.6 },
+        {
+            text = "*Yay!*",
+            x = 290, y = 15,
+            rect =  {width = 90, height = 123},
+            parallax = 0.6
+        },
+    }
+    },
+
+    {      -- Panel 16
+    frame = { width = 1100 },
+    layers = {
+        { image = "Intro/P/Background.png", x = 150,     parallax = 0.5 },
+        { image = "Intro/P/Flip.png", x = 200, parallax = 0.45 },
+        { image = "Intro/P/Nuts.png", x = 200, parallax = 0.4 },
+        { image = "Intro/P/Speech.png",   x = 250, parallax = 0.45 },
+        {
+            text = "*Let's pick up some nuts*",
+            x = 300, y = 45,
+            rect =  {width = 90, height = 123},
+            parallax = 0.45
+        },
+    }
+    },
     }
 }
 
