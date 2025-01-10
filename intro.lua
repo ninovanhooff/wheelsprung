8687f4d223e50d14bbce66e954b4c3af8acff872
Anders Bjørnskov <andersnormannmadsen@gmail.com>
2025-01-10 21:08:36 +0100

    Use the crank indicator

--- intro.lua
+++ intro.lua
@@ -1,24 +1,34 @@
+import "CoreLibs/ui"
+
 Intro = {
     -- Sequence 1
     title = "Introduction",
     panels = { -- a list of PANELS for Sequence 1
         {      -- Panel 1
+            renderFunction = function(panel, offset)
+                for i, layer in ipairs(panel.layers) do
+                    Panels.renderLayerInPanel(layer, panel, offset)
+                end
+                if math.abs(offset.x) < 20 then
+                    playdate.ui.crankIndicator:draw()
+                end
+            end,
             layers = {
                 { image = "Intro/A/1-shop.png",     parallax = 1 },
                 { image = "Intro/A/2-parents.png",  parallax = 0.8 },
                 { image = "Intro/A/3-children.png", parallax = 0.6 },
                 { image = "Intro/A/4-plant.png",    parallax = 0.1 },
-                { 
-                    imageTable = "common/buttonRT", 
-                    x = 330, y = 100, parallax = 2, 
-                    loop = true, startDelay = 1500,
-                },
+                -- { 
+                --     imageTable = "common/buttonRT", 
+                --     x = 330, y = 100, parallax = 2, 
+                --     loop = true, startDelay = 1500,
+                -- },
                 {
                     text = "Ⓑ: Skip Story",
                     x = 10, y = 200, parallax = 0,
                     background = Panels.Color.WHITE,
                     animate = {
-                        trigger = Panels.Input.RIGHT,
+                        scrollTrigger = 0.55,
                         opacity = 0,
                         duration = 1000,
                         ease = playdate.easingFunctions.outQuint,
