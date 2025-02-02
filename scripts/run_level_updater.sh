e4d065dc2f3f8e70556b8701e70ecb6684a9aa2a
Nino van Hooff <ninovanhooff@gmail.com>
2025-02-02 13:38:08 +0100

    ADD check for level salt and check length

--- scripts/run_level_updater.sh
+++ scripts/run_level_updater.sh
@@ -5,6 +5,12 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 . "$SCRIPT_DIR/set_release_env.sh"
 
-echo "level salt run_updater $WHEELSPRUNG_LEVEL_SALT"
+if [ ${#WHEELSPRUNG_LEVEL_SALT} -ne 64 ]; then
+  echo "Error: WHEELSPRUNG_LEVEL_SALT must be 64 characters long."
+  exit 1
+fi
+
+## only print the first 7 characters of the salt for security reasons
+echo "level salt run_updater ${WHEELSPRUNG_LEVEL_SALT:0:7}"
 
 nim c -d:useHostOS -r "./scripts/level_hash_updater/update_level_hashes.nim"
