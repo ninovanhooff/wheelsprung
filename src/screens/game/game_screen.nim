0efc92efa1a8cafd8646acd26a9c3c1599ed774d
Nino van Hooff <ninovanhooff@gmail.com>
2025-02-02 14:08:45 +0100

    CHANGE don't add menu itemns when playdate env not available

--- src/screens/game/game_screen.nim
+++ src/screens/game/game_screen.nim
@@ -293,6 +293,8 @@ proc updateTimers(state: GameState) =
     echo("blinker timeout")
 
 proc addMenuItems(gameScreen: GameScreen) =
+  when defined(useHostOS):
+    return
   if gameScreen.state.isInLiveMode:
     # discard playdate.system.addMenuItem(settingsLabel, proc(menuItem: PDMenuItemButton) =
     #   pushScreen(newSettingsScreen())
